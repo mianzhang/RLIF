e8a8250c55ac51199f8d73ab462f6480ea276824
mianz <mianz@mianzs-MacBook-Air.local>
2025-09-05 12:46:08 -0500

    add_ifeval_ifbench_reward_func

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -101,6 +101,14 @@ def default_compute_score(
         from . import search_r1_like_qa_em
 
         res = search_r1_like_qa_em.compute_score(solution_str, ground_truth)
+    elif data_source == "ifbench":
+        from . import ifbench
+
+        res = ifbench.compute_score(solution_str, ground_truth)
+    elif data_source == "ifeval":
+        from . import ifeval
+
+        res = ifeval.compute_score(solution_str, ground_truth)
 
     else:
         raise NotImplementedError(f"Reward function is not implemented for {data_source=}")
